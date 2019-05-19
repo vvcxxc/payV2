3d06791fefc338a2498bc39fd82832eee9ef0821
songyizhe <769724661@qq.com>
2019-05-19 15:33:26 +0800

    测试2

--- src/utils/handle_login.js
+++ src/utils/handle_login.js
@@ -56,7 +56,7 @@ export const Login = () => {
             let url = baseurl+'/wechat/wxoauth?code_id='+codeid+'&from=onescan'
             
             url = encodeURIComponent(url)
-            // window.location.href = 'http://wxauth.tdianyi.com/index.html?appid=wxecdd282fde9a9dfd&redirect_uri='+url+'&response_type=code&scope=snsapi_userinfo&connect_redirect=1&state=STATE&state=STATE'
+            window.location.href = 'http://wxauth.tdianyi.com/index.html?appid=wxecdd282fde9a9dfd&redirect_uri='+url+'&response_type=code&scope=snsapi_userinfo&connect_redirect=1&state=STATE&state=STATE'
         }
     }else if(browsertype == 'alipay'){
         let token = Cookie.get(_token);
