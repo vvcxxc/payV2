ab0fcf015bb8235cc0fb783a4268805a41276543
songyizhe <769724661@qq.com>
2019-06-22 10:27:27 +0800

    回退

--- src/components/x-keyboard.vue
+++ src/components/x-keyboard.vue
@@ -157,6 +157,8 @@ export default {
                   _this.$router.push({name:'activity',params:message})
                   } 
               });
+            }else{
+              this.$router.push({name:'activity',params:message})
             }
           }else if(browsertype == 'alipay'){
             // 支付宝支付
@@ -196,6 +198,8 @@ export default {
                   })
                 }
               })
+            }else{
+              this.$router.push({name:'activity',params:message})
             }
             
             }
