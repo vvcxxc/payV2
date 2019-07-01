2d2a9dda53e433626a08ebe0e018110747aa4a41
songyizhe <769724661@qq.com>
2019-07-01 19:25:17 +0800

    图

--- src/views/pay.vue
+++ src/views/pay.vue
@@ -224,7 +224,7 @@
 								let url =  process.env.VUE_APP_BASE_DOMAIN + 'wechat/wxoauth?code_id='+codeid+'&from=' + from;
 								url = encodeURIComponent(url);
 								let urls = 'http://wxauth.tdianyi.com/index.html?appid=wxecdd282fde9a9dfd&redirect_uri='+url+'&response_type=code&scope=snsapi_userinfo&connect_redirect=1&state=STATE&state=STATE';
-								return window.location.href = urls;
+								// return window.location.href = urls;
 							}else if(browsertype == 'alipay'){
 								let url = process.env.VUE_APP_BASE_DOMAIN +"ali/getZfbUserInfo";
 								let codeid = getUrlParams().code_id;
