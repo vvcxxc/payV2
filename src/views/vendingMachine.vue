2fe3ec561cac99acf5e8164dbed62a54ef9b5917
songyizhe <769724661@qq.com>
2019-07-09 15:54:12 +0800

    a

---
+++ src/views/vendingMachine.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="payBox">
+        <!-- 广告 -->
+        <div class="area-AD">
+            <img :src="ads.pic" alt />
+        </div>
+        <div class="info">
+            <div>
+                <p>金额</p>
+                <p>￥3000.00</p>
+            </div>
+            <div>
+                <p>商家</p>
+                <p>得利有限公司</p>
+            </div>
+            <div>
+                <p>商品</p>
+                <p>得力文具03号铅笔橡皮套装</p>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
+
