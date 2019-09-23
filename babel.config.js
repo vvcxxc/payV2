91ac74d80a3118bc9b95362a42c5303cc913d0ca
songyizhe <769724661@qq.com>
2019-09-23 10:32:43 +0800

    测试

--- babel.config.js
+++ babel.config.js
@@ -8,24 +8,6 @@ module.exports = {
       libraryName: 'vant',
       libraryDirectory: 'es',
       style: true
-    }, 'vant'],
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-      new UglifyJsPlugin({
-        uglifyOptions: {
-          compress: {
-            drop_debugger: true,
-            drop_console: true
-          }
-        },
-        sourceMap: false,
-        parallel: true
-      }),
-      // 配置compression-webpack-plugin压缩
-      new CompressionWebpackPlugin({
-        algorithm: 'gzip',
-        test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
-        threshold: 10240,
-        minRatio: 0.8
-      })
+    }, 'vant']
   ]
 }
