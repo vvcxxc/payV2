e569c68282d64a620a4876187721d66b2b8cd543
songyizhe <769724661@qq.com>
2019-11-15 15:15:06 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
-
+import * as Sentry from '@sentry/browser';
+import * as Integrations from '@sentry/integrations';
 import router from './router'
 import store from './store'
 import FastClick from 'fastclick';
 import Vconsole from 'vconsole'
+Sentry.init({
+  dsn: 'http://7e203dd3d7fc40e292addab1cba910c0@sentry.tdianyi.com/2',
+  integrations: [new Integrations.Vue({Vue, attachProps: true})],
+});
 if(process.env.VUE_APP_FLAG == 'test' || process.env.VUE_APP_FLAG == 'release'){
   let vConsole = new Vconsole()
   Vue.use(vConsole)
