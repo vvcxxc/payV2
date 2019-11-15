f2299bd8a8ce5fbc6c63575fa643b80730cd6d5e
songyizhe <769724661@qq.com>
2019-11-15 15:52:39 +0800

    分环境测试

--- src/views/pay.vue
+++ src/views/pay.vue
@@ -195,6 +195,7 @@ export default {
   },
 
   created() {
+    console.log(this.a())
     let type = process.env.NODE_ENV;
     if (type == "development") {
       Cookie.set("test_open_id", "oy6pQ05896O22gUAljVH4uqvCnhU");
