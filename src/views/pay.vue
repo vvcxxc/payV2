329089c67496db459c6ca9117f8071a230b2d57f
songyizhe <769724661@qq.com>
2019-11-15 16:29:01 +0800

    a

--- src/views/pay.vue
+++ src/views/pay.vue
@@ -195,7 +195,6 @@ export default {
   },
 
   created() {
-    this.b()
     let type = process.env.NODE_ENV;
     if (type == "development") {
       Cookie.set("test_open_id", "oy6pQ05896O22gUAljVH4uqvCnhU");
