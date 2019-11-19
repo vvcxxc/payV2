9e53f64451795dfb50f39153e3d1a249c9aa3209
songyizhe <769724661@qq.com>
2019-11-19 17:36:56 +0800

    错误日志

--- src/main.js
+++ src/main.js
@@ -7,10 +7,13 @@ import router from './router'
 import store from './store'
 import FastClick from 'fastclick';
 import Vconsole from 'vconsole'
-Sentry.init({
-  dsn: 'http://7e203dd3d7fc40e292addab1cba910c0@sentry.tdianyi.com/2',
-  integrations: [new Integrations.Vue({Vue, attachProps: true})],
-});
+if(process.env.VUE_APP_FLAG == 'test' || process.env.VUE_APP_FLAG == 'release' || process.env.VUE_APP_FLAG == 'production'){
+  Sentry.init({
+    dsn: 'http://7e203dd3d7fc40e292addab1cba910c0@sentry.tdianyi.com/2',
+    integrations: [new Integrations.Vue({Vue, attachProps: true})],
+    environment: process.env.VUE_APP_FLAG
+  });
+}
 if(process.env.VUE_APP_FLAG == 'test' || process.env.VUE_APP_FLAG == 'release'){
   let vConsole = new Vconsole()
   Vue.use(vConsole)
