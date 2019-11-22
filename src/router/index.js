ad0377d80c92d09574862115efee3569e51cff58
songyizhe <769724661@qq.com>
2019-11-22 15:18:55 +0800

    a

--- src/router/index.js
+++ src/router/index.js
@@ -52,7 +52,7 @@ var router = new VueRouter({
 // 路由守卫
 router.beforeEach((to,from,next) => {
   if(process.env.VUE_APP_FLAG == 'development'){
-    Cookie.set('test_token_auth','eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vdGVzdC5hcGkudGRpYW55aS5jb20vd2VjaGF0L3d4b2F1dGgiLCJpYXQiOjE1NzQzOTE3MjksImV4cCI6MTU3NDM5NzcyOSwibmJmIjoxNTc0MzkxNzI5LCJqdGkiOiJVRnR0VkU5V3R6TkZIYnpzIiwic3ViIjo1MzQ1LCJwcnYiOiJmNmI3MTU0OWRiOGMyYzQyYjc1ODI3YWE0NGYwMmI3ZWU1MjlkMjRkIn0.LJETUDW8Q7Oig9mwX1xcsg_TFBUQHiaK0WlwKx6Uek4')
+    Cookie.set('test_token_auth','eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vdGVzdC5hcGkudGRpYW55aS5jb20vd2VjaGF0L3d4b2F1dGgiLCJpYXQiOjE1NzQ0MDcwMjYsImV4cCI6MTU3NDc2NzAyNiwibmJmIjoxNTc0NDA3MDI2LCJqdGkiOiJlY3dmQmZUZEVxVVRNc2plIiwic3ViIjo1MzQ1LCJwcnYiOiJmNmI3MTU0OWRiOGMyYzQyYjc1ODI3YWE0NGYwMmI3ZWU1MjlkMjRkIn0.5S_B7YCCde93tJV4WTGfM4h3b9FgcGudu8vUtpKrCWI')
   }
   if(to.name == 'pay'){
     if (
