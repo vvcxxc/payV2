3c6970b4a41feb9a6537ab66f8fc7ec39a14080c
songyizhe <769724661@qq.com>
2020-01-06 10:43:41 +0800

    字体大小

--- src/main.js
+++ src/main.js
@@ -12,22 +12,20 @@ import {
 } from 'vant';
 Vue.use(Popup);
 
-(function(){
-  var $dom = document.createElement('div');
-  $dom.style = 'font-size:10px;';
-  document.body.appendChild($dom);
-  // 计算出放大后的字体
-  var scaledFontSize = parseInt(window.getComputedStyle($dom, null).getPropertyValue('font-size'));
-  document.body.removeChild($dom);
-  // 计算原字体和放大后字体的比例
-  var scaleFactor = 10 / scaledFontSize;
-
-  // 取html元素的字体大小
-  // 注意，这个大小也经过缩放了
-  // 所以下方计算的时候 *scaledFontSize是原来的html字体大小
-  // 再次 *scaledFontSize才是我们要设置的大小
-  var originRootFontSize = parseInt(window.getComputedStyle(document.documentElement, null).getPropertyValue('font-size'));
-  document.documentElement.style.fontSize = originRootFontSize * scaleFactor * scaleFactor + 'px';
+(function() {
+  if (typeof WeixinJSBridge == "object" && typeof WeixinJSBridge.invoke == "function") {
+      handleFontSize();
+  } else {
+      document.addEventListener("WeixinJSBridgeReady", handleFontSize, false);
+  }
+  function handleFontSize() {
+      // 设置网页字体为默认大小
+      WeixinJSBridge.invoke('setFontSizeCallback', { 'fontSize' : 0 });
+      // 重写设置网页字体大小的事件
+      WeixinJSBridge.on('menu:setfont', function() {
+          WeixinJSBridge.invoke('setFontSizeCallback', { 'fontSize' : 0 });
+      });
+  }
 })();
 
 if (process.env.VUE_APP_FLAG == 'test' || process.env.VUE_APP_FLAG == 'release' || process.env.VUE_APP_FLAG == 'production') {
